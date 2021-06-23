b304589e35460e3ed7d99c1219b784acfa8cc499
carlosribe <52242005+carlosribe@users.noreply.github.com>
2021-06-23 01:33:51 +0000

    listagem de produtos

--- src/main/java/br/univille/carlosribeirodacs2021/controller/ProdutoController.java
+++ src/main/java/br/univille/carlosribeirodacs2021/controller/ProdutoController.java
@@ -1,17 +1,28 @@
 package br.univille.carlosribeirodacs2021.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import br.univille.carlosribeirodacs2021.model.Produto;
+import br.univille.carlosribeirodacs2021.service.ProdutoService;
 
 @Controller
 @RequestMapping("/produto")
 
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoService service;
+
     @GetMapping      
     public ModelAndView index(){
-        return new ModelAndView("produto/index");
+
+        List<Produto> listaProdutos = service.getAllProdutos();
+
+        return new ModelAndView("produto/index", "listaProdutos", listaProdutos);
     }
 }
