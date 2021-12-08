13c0fe1346d066b4880fd22b7a1f766fea066da8
carlos <carlosferreira.jlle@gmail.com>
2021-12-07 22:13:41 -0300

    Update new files

---
+++ src/main/java/br/univille/carlosribeirodacs2021/repository/VendaRepository.java
@@ -0,0 +1,10 @@
+package br.univille.carlosribeirodacs2021.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.univille.carlosribeirodacs2021.model.Venda;
+
+@Repository
+public interface VendaRepository extends JpaRepository<Venda,Long>{   
+    
+}
---
+++ src/test/java/br/univille/carlosribeirodacs2021/Carlosribeirodacs2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.carlosribeirodacs2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Carlosribeirodacs2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
